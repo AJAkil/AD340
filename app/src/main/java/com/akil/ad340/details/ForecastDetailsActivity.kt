3a0f58b7fae985bcd5b94fc504e353edd0d21f3b
AJAkil <cyclosis47@gmail.com>
2020-07-28 01:52:09 +0600

    Edited View of new Activity

--- app/src/main/java/com/akil/ad340/details/ForecastDetailsActivity.kt
+++ app/src/main/java/com/akil/ad340/details/ForecastDetailsActivity.kt
@@ -2,11 +2,22 @@ package com.akil.ad340.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.akil.ad340.R
 
 class ForecastDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forecast_details)
+
+        // set the title of the activity with setTitle()
+        setTitle(R.string.forecast_details)
+
+        val tempText = findViewById<TextView>(R.id.tempText)
+        val descriptionText = findViewById<TextView>(R.id.descriptionText)
+
+        tempText.text = "81.2°"
+        descriptionText.text = "Mostly Sunny"
+
     }
 }
